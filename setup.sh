fe8d62694ec3edc672a58bda4f541bcd705c409c
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2021-01-14 20:19:16 -0300

    WIP refactor to use tinycoffee as a engine

--- setup.sh
+++ setup.sh
@@ -16,5 +16,12 @@ cd ../../
 
 echo "setup-tinycoffee: setup done"
 
+# download tico
 wget -O tico.nelua https://raw.githubusercontent.com/Andre-LA/tico-nelua/main/output/tico.nelua
+
+# download nprof
 wget -O nprof.nelua https://raw.githubusercontent.com/Andre-LA/nprof/master/nprof.nelua
+
+
+# clone rotor
+git clone https://github.com/Andre-LA/Rotor-nelua.git
---
+++ tests/lunar-lander/.neluacfg.lua
@@ -0,0 +1,8 @@
+return {
+  add_path = {
+    '../../nprof',
+    '../../Rotor-nelua',
+    '../../tico-nelua/output',
+    '../../', -- rotor quick
+  }
+}
