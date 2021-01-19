076c87302d7d2e992438030143f51ac6c9b009b5
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2021-01-19 20:59:46 -0300

    WIP: SDL2 refactor
    For now I'm creating a basic micro (nano?) game framework (quickgamelib)
    and game math library (gamemath).
    
    The "intersection test" and "basic gravity" are also refactored to use
    quickgamelib.
    
    I still want to use tinycoffee, but is still a WIP library while SDL2 is
    battle tested, which is something I need to make a new game soon.
    
    I may make "quickgamelib" compatible with both SDL2 and tinycoffee in
    the future, so the transition between the two libraries will be easier.

--- setup.sh
+++ setup.sh
@@ -1,27 +1,8 @@
+rm -rf Rotor-nelua
 rm nprof.nelua
-rm tico.nelua
-rm -rf tinycoffee/
-
-# clone tinycoffee
-git clone https://github.com/canoi12/tinycoffee.git
-cd tinycoffee
-
-# build tinycoffee
-mkdir build
-cd build
-cmake ..
-make
-
-cd ../../
-
-echo "setup-tinycoffee: setup done"
-
-# download tico
-wget -O tico.nelua https://raw.githubusercontent.com/Andre-LA/tico-nelua/main/output/tico.nelua
 
 # download nprof
 wget -O nprof.nelua https://raw.githubusercontent.com/Andre-LA/nprof/master/nprof.nelua
-
 
 # clone rotor
 git clone https://github.com/Andre-LA/Rotor-nelua.git
